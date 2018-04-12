48d0775b6b0a46efb4c20cb32712f1ee6db6378a
bcrsp97 <brandon-18@hotmail.es>
2018-04-12 13:03:06 +0200

    dsd

--- js/index.js
+++ js/index.js
@@ -9,4 +9,15 @@ $(document).ready(function() {
 
 function loadInitialData(){
 	var canvas = document.getElementById("stageDamas");
+    ctx = canvas.getContext("2d"); 
+    
+    //canvas click
+    canvas.onclick = function(value) {        
+        processRound(value);
+        showCurrentPlayer();
+    }
+}
+
+function processRound(){
+	
 }
